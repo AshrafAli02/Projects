9ec0e79017c2f33c2ee41614808744a43d3095cf
AshrafAli <ashrafaliaaa94@gmail.com>
2021-04-03 21:49:05 +0530

    BMI

---
+++ BMI-Calculator/BMI-Calculator/BMI.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI_Calculator
+{
+    class BMI
+    {
+        private int _age;
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                _age = value;
+            }
+        }
+        private double _height;
+        public double Height
+        {
+            get
+            {
+                return _height;
+            }
+            set
+            {
+                _height=value;
+            }
+        }
+        private int _weight;
+        public int Weight
+        {
+            get
+            {
+                return _weight;
+            }
+            set
+            {
+                _weight=value;
+            }
+        }
+        private string _gender;
+        public string Gender
+        {
+            get
+            {
+                return _gender;
+            }
+            set
+            {
+                 _gender=value;
+            }
+        }
+        private double _bmi;
+        public double _BMI
+        {
+            get
+            {
+                return _bmi;
+            }
+            
+        }
+        public double casecheck(int v)
+        {
+            double value = 0;
+            switch(v)
+            {
+                case 1:
+                    value = BMICalulate(_height, _weight);
+                    break;
+                case 2:
+                    value = BMIinFeet(_height, _weight);
+                    break;
+            }
+            return value;
+        }
+        public double BMICalulate(double H,int W)
+        {
+            double value = 0;
+            double  h = H *0.01;
+            value= W / (h * h);
+            return value;
+        }
+        
+        public double BMIinFeet(double H,int W)
+        {
+            double value = 0;
+            double h = H / 30.48;
+            value = W / (h * h);
+            return value;
+        }
+      
+    }
+}
---
+++ BMI-Calculator/BMI-Calculator/SIUnit.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BMI_Calculator
+{
+    /// <summary>
+    /// Interaction logic for SIUnit.xaml
+    /// </summary>
+    /// 
+    public partial class SIUnit : Window
+    {
+        BMI bmical = new BMI();
+        public SIUnit()
+        {
+            InitializeComponent();
+        }
+
+       public void setvalue()
+        {
+            bmical.Age= int.Parse(AgeBox.Text);
+            bmical.Gender = "";
+            if (Mradio.IsChecked == true)
+            {
+                bmical.Gender= "Male";
+            }
+            else
+            {
+                bmical.Gender= "Female";
+            }
+            bmical.Height= int.Parse(HeightBox.Text);
+            bmical.Weight= int.Parse(WeightBox.Text);
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            setvalue();
+            int cases = UnitSelection.SelectedIndex;
+            int dummy=(int) bmical.casecheck(cases);
+            double BmiValue = bmical._BMI;
+            BMIt.Text = dummy.ToString();
+        
+            if ((int)dummy < 16)
+            {
+                //BackColor = "Red";
+                DisplayPanel.Background =new LinearGradientBrush(Colors.Red,Colors.Pink,90);
+            }
+            else if ((int)dummy >= 16 && (int)dummy <= 17)
+            {
+                //BackColor = "Green";
+                DisplayPanel.Background = new LinearGradientBrush(Colors.Green, Colors.Pink, 90);
+            }
+            else if ((int)dummy >= 18 && (int)dummy <= 25)
+            {
+                //BackColor = "Lightblue";
+                DisplayPanel.Background = new LinearGradientBrush(Colors.LightBlue, Colors.Pink, 90);
+            }
+            else
+            {
+                //BackColor = "Orange";
+                DisplayPanel.Background = new LinearGradientBrush(Colors.Orange, Colors.Pink, 90);
+            }
+
+        }
+    }
+}
