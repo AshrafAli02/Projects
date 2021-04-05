27e520614c16eaf929c107c84dbcaed3461e7a6d
AshrafAli <ashrafaliaaa94@gmail.com>
2021-04-05 16:58:19 +0530

    Loan Calculator

---
+++ loan-calculator/loan-calculator/Loan.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loan_calculator
+{
+    class Loan
+    {
+        public Loan()
+        {
+            this._loanAmount = 0;
+            this._interestPercentage = 0;
+            this._noofMonths = 0;
+        }
+        private int _loanAmount;
+        public int LoanAmount
+        {
+            get
+            {
+                return _loanAmount;
+            }
+            set
+            {
+                _loanAmount=value;
+            }
+        }
+        private double _interestPercentage;
+        public double InterestPercentage
+        {
+            get
+            {
+                return _interestPercentage;
+            }
+            set
+            {
+                _interestPercentage=value;
+            }
+        }
+        private int _noofMonths;
+        public int NumberOfMonths
+        {
+            get
+            {
+                return _noofMonths;
+            }
+            set
+            {
+                _noofMonths=value;
+            }
+        }
+        
+
+        public int Emicalculate()
+        {
+            double Value=0;
+            double interest = (_interestPercentage*0.01)/12;
+            double cal =Math.Pow((1 + interest), _noofMonths);
+            double v = cal / (cal - 1);
+            Value = (_loanAmount * interest) * v;
+            return (int)Value;
+        }
+    }
+}
---
+++ loan-calculator/loan-calculator/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace loan_calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Loan l = new Loan();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateEmi_Click(object sender, RoutedEventArgs e)
+        {
+            ValueSet();
+            Emi.Text =" Rs: " +l.Emicalculate().ToString()+"/-";
+        }
+        public void ValueSet()
+        {
+            l.LoanAmount = int.Parse(LoanAmount.Text);
+            l.InterestPercentage = double.Parse(InterestPercentage.Text);
+            l.NumberOfMonths = int.Parse(NoofMonths.Text);
+        }
+    }
+}
