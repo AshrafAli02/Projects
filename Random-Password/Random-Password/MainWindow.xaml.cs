73d517b45191c73ed72c6a325d5a315cb336f251
AshrafAli <ashrafaliaaa94@gmail.com>
2021-04-06 21:32:18 +0530

    Random Password

---
+++ Random-Password/Random-Password/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Random_Password
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            NumberCheckBox.IsChecked = true;
+            LowercaseCheckBox.IsChecked = true;
+            UpperCaseCheckBox.IsChecked = true;
+            SymbolCheckBox.IsChecked = true;
+            PasswordBox.Text = "Your Password";
+        }
+
+        public void Hello()
+        {
+            MessageBox.Show("Hello");
+        }
+
+        private void Clipboardbtn_Click(object sender, RoutedEventArgs e)
+        {
+           
+            PasswordBox.Focusable = true;
+            if (PasswordBox.Text == "Your Password")
+            {
+               MessageBox.Show("Generate Your Password!...","Warning",MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+            else
+            {
+                Clipboard.SetText(PasswordBox.Text);
+                MessageBox.Show("Text Copied to Clipboard ...","Success",MessageBoxButton.OK,MessageBoxImage.Information);
+            }
+            
+        }
+        Password pass = new Password();
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            pass.Count = 0;
+            pass.Length = int.Parse(Lengthbox.Text);
+            var v2 = UpperCaseCheckBox.IsChecked;
+            var v1 = NumberCheckBox.IsChecked;
+            var v3 = LowercaseCheckBox.IsChecked;
+            var v4 = SymbolCheckBox.IsChecked;
+            if (v1 == false && v2 == false && v3 == false && v4 == false||(v1 == false && v2 == false && v3 == false && v4 == false))
+            {
+                MessageBox.Show("Select Atleast One Pattern", "Warning", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                pass.GenaratePassword = new char[int.Parse(Lengthbox.Text)];
+                pass.passGenerate(v1, v2, v3, v4);
+                PasswordBox.Text = pass.getPassword();
+            }
+            
+        }
+
+        private void Lengthbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(int.Parse(Lengthbox.Text)<=8)
+            {
+                Passwordstrength.Background = new SolidColorBrush(Colors.Red);
+            }
+            if (int.Parse(Lengthbox.Text) >= 12)
+            {
+                Passwordstrength.Background = new SolidColorBrush(Colors.Green);
+            }
+            else if(Lengthbox.Text=="")
+            {
+                
+            }
+        }
+    }
+}
---
+++ Random-Password/Random-Password/Password.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_Password
+{
+    class Password
+    {
+        public char[] GenaratePassword;
+        public List<char> passlist = new List<char>();
+        private string _password;
+
+        public string _Password
+        {
+            get
+            {
+                return _password;
+            }
+        }
+        private int _length;
+        public int Length
+        {
+            get
+            {
+                return _length;
+            }
+            set
+            {
+                _length = value;
+            }
+        }
+        private int _count;
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+            set
+            {
+                _count = value;
+            }
+        }
+        public Password()
+        {
+            this._password = "";
+            this._length = 0;
+            this.GenaratePassword = new char[_length];
+            this._count = 0;
+        }
+
+
+       
+        public void GetChar(int s,int l,int len)
+        {
+            Random number = new Random();
+            for (int i = 0; i <len; i++)
+            {
+                int value = number.Next(s,l);
+                GenaratePassword[_count]=(char.Parse(char.ConvertFromUtf32(value)));
+                _count++;
+            }
+
+        }
+
+        public string getPassword()
+        {
+            Suffle();
+            StringBuilder sb = new StringBuilder();
+            foreach(char c in GenaratePassword)
+            {
+                sb.Append(c);
+            }
+            return sb.ToString();
+        }
+        public void Suffle()
+        {
+            Random ra = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                int number = ra.Next(_length);
+                int num = ra.Next(_length);
+                passSuffle(number, num);
+            }
+        }
+
+        public void passSuffle(int First, int Last)
+        {
+            while (First <= Last)
+            {
+                char temp = GenaratePassword[First];
+                GenaratePassword[First] = GenaratePassword[Last];
+                GenaratePassword[Last] = temp;
+                First++;
+                Last--;
+            }
+
+        }
+        public void passGenerate(bool? v1, bool? v2, bool? v3, bool? v4)
+        {
+            int l = 0;
+            if (v1 == true) { l += 1; }
+            if (v2 == true) { l += 1; }
+            if (v3 == true) { l += 1; }
+            if (v4 == true) { l += 1; }
+            int sub = _length / l;
+            if(_length==sub*l)
+            {
+                if(v1==true)
+                {
+                    GetChar(48, 57, sub);
+                }
+                if(v2==true)
+                {
+                    GetChar(97, 122,sub);
+                }
+                if(v3==true)
+                {
+                    GetChar(65, 90, sub);
+                }
+                if(v4==true)
+                {
+                    GetChar(33, 47, sub);
+                }
+            }
+            else if(_length != sub * l)
+            {
+                if (v1 == true)
+                {
+                    GetChar(48, 57, sub);
+                }
+                if (v2 == true)
+                {
+                    GetChar(97, 122, sub);
+                }
+                if (v3 == true)
+                {
+                    GetChar(65, 90, sub);
+                }
+                if (v4 == true)
+                {
+                    GetChar(33, 47, sub);
+                }
+                int rem = Length % l;
+                GetChar(33, 122, rem);
+
+            }
+        }
+    }
+}
